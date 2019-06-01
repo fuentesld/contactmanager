ef5b69f7eaaaaaa607ad9d10003c642904654a1c
Luis Fuentes <fuentesld@gmail.com>
2019-06-01 12:28:43 -0500

    Updates

---
+++ precache-manifest.78dee8f27988790c45ec8b2486ea9acf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "645fddc0c7b067019714561bf72c6be4",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "fc8ea03b2eded05c5a5d",
+    "url": "/contactmanager/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "0ae94483da0b5021993c",
+    "url": "/contactmanager/static/css/main.c38c418a.chunk.css"
+  },
+  {
+    "revision": "fc8ea03b2eded05c5a5d",
+    "url": "/contactmanager/static/js/2.ff95717f.chunk.js"
+  },
+  {
+    "revision": "0ae94483da0b5021993c",
+    "url": "/contactmanager/static/js/main.22d1bd09.chunk.js"
+  },
+  {
+    "revision": "a9b80a0b0e722e0d3664",
+    "url": "/contactmanager/static/js/runtime~main.884ecd02.js"
+  }
+]);
